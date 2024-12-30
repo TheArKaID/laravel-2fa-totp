0a15f3ac0d15ea2d4ffedd1d8f42ff7c75b528fd
Arifia Kasastra R <arka.progammer@gmail.com>
2024-12-30 16:06:34 +0700

    Implement two-factor authentication logout functionality and redirect to verification page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,20 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        Auth::logout();
+
+        $request->session()->put('twofa:user:id', $user->id);
+        return redirect()->route('2fa.verification');
     }
 }
