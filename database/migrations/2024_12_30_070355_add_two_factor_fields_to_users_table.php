0a2d4fca0b8f694b6af842deab8477fe722fe61d
Arifia Kasastra R <arka.progammer@gmail.com>
2024-12-30 14:09:21 +0700

    Add two-factor authentication fields to User model and database migration

---
+++ database/migrations/2024_12_30_070355_add_two_factor_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('twofa_secret')->nullable();
+            $table->string('twofa_qr')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('twofa_secret');
+            $table->dropColumn('twofa_qr');
+        });
+    }
+};
